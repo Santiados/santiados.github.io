637863f0da150f8c493cfa06e83582afb34a3a1b
Santiados <santireven20@gmail.com>
2019-01-07 02:39:29 +0100

    spy_added

--- js/index.js
+++ js/index.js
@@ -2,12 +2,27 @@
 // Date: 10-12-2018
 
 
-(function(){
-    $(document).scroll(function (e) { 
-        if($(this).scrollTop() > 120){
+(function () {
+    $(document).scroll(function (e) {
+        if ($(this).scrollTop() > 120) {
             $('header').addClass('toggle-header');
         } else {
             $('header').removeClass('toggle-header');
         }
     });
 })();
+
+function scrollTo(selector = null) {
+    var many = 0;
+    if (!selector || $(selector).length < 1) return;
+    many = $(selector).offset().top - $('header').outerHeight();
+    $('html,body').animate({
+        scrollTop: many
+    }, 500);
+};
+
+
+$('.spy').click(function (e) {
+    var target = $(this).attr('scrollTo');
+    scrollTo(target);
+});
